28595ac3cd7dd6e723df63fa40f0190ac5cc781a
Amy Parker <amy@amyip.net>
2023-07-25 21:14:10 -0700

    dbhandle: create dbapi interface
    This patch initializes the sqliteh file (not in a usable state currently), and creates an interface for DBAPI. Any DBAPI compliant module can simply just use the existing implementations. Future work may be required to ensure there are no dialect-based conflicts.
    
    Signed-off-by: Amy Parker <amy@amyip.net>

--- srv/dbhandle.py
+++ srv/dbhandle.py
@@ -58,3 +58,120 @@ class DBAbstract:
         
     def update_cached_stats(self, srv: str, st: int) -> None:
         unimplemented()
+
+
+class DBAPIAbstracted(DBAbstract):
+    def __init__(self, config: dict, prefix: str) -> None:
+        super().__init__(config=config)
+        self.p = prefix
+
+    def _get_ups(self, srv: str, conn) -> dict:
+        curr = conn.cursor()
+        sql = f"select monthlyUptime, yearlyUptime, allTimeUptime, currentStatus \
+                from {self.p}cached_stats \
+                where serverName = %s;"
+        curr.execute(sql, (srv,))
+        row = curr.fetchone()
+        if row is None:
+            curr.close()
+            raise Exception("ssvp: mysql: invalid server")
+        curr.close()
+        conn.close()
+        return {
+            "monthly_uptime": row[0],
+            "yearly_uptime": row[1],
+            "alltime_uptime": row[2],
+            "current_status": row[3]
+        }
+
+    def _fdd(self, srv: str, conn) -> dict:
+        curr = conn.cursor()
+        sql = f"select logDate, serverStatus \
+                from {self.p}day_logs \
+                where logDate between (current_date() - interval 3 month) and current_date() and serverName = %s;"
+        curr.execute(sql, (srv,))
+        res = {}
+        for row in curr.fetchall():
+            res[row[0]] = row[1]
+        curr.close()
+        conn.close()
+        return res
+
+    def _hdr(self, srv: str, st: int, conn) -> int:
+        curr = conn.cursor()
+        day = datetime.date.today()
+        sql = f"select exists( \
+                    select serverName \
+                    from {self.p}day_logs \
+                    where serverName = %s \
+                        and logDate = %s \
+                );"
+        curr.execute(sql, (srv, day))
+        rc_exists = curr.fetchone()[0]
+        sql = f"select max( \
+                    select severity \
+                    from {self.p}events \
+                    where serverName = %s \
+                        and endTime is not null \
+                );"
+        mx = None
+        try:
+            curr.execute(sql, (srv,))
+            mx = max(st, curr.fetchone()[0])
+        # we can ignore the pep violation
+        except:
+            mx = st
+        if not rc_exists:
+            sql = f"insert into {self.p}day_logs \
+                    (logDate, serverName, serverStatus) values \
+                    (%s, %s, %s);"
+            curr.execute(sql, (day, srv, mx))
+        else:
+            sql = f"update {self.p}day_logs \
+                    set serverStatus = %s \
+                    where logDate = %s \
+                        and serverName = %s;"
+            curr.execute(sql, (mx, day, srv))
+        curr.fetchall()
+        conn.commit()
+        curr.close()
+        conn.close()
+        return mx
+
+    def _iil(self, srv: str, status: int, conn) -> None:
+        curr = conn.cursor()
+        sql = f"insert into {self.p}interval_logs \
+                (logDate, serverName, serverStatus) values \
+                (%s, %s, %s);"
+        curr.execute(sql, (datetime.datetime.now(), srv, status))
+        curr.fetchall()
+        conn.commit()
+        curr.close()
+        conn.close()
+
+    def _ucs(self, srv: str, st: int, conn) -> None:
+        curr = conn.cursor()
+        sql = f"update {self.p}cached_stats \
+                set monthlyUptime = \
+                    (select AVG(NOT serverStatus) \
+                     from {self.p}interval_logs \
+                     where logDate between \
+                        (CURRENT_DATE() - interval 1 month) and CURRENT_DATE() \
+                        and serverName = %s), \
+                    yearlyUptime = \
+                    (select AVG(NOT serverStatus) \
+                     from {self.p}interval_logs \
+                     where logDate between \
+                        (CURRENT_DATE() - interval 1 year) and CURRENT_DATE() \
+                        and serverName = %s), \
+                    allTimeUptime = \
+                    (select AVG(NOT serverStatus) \
+                     from {self.p}interval_logs \
+                     where serverName = %s), \
+                    currentStatus = %s\
+                where serverName = %s;"
+        curr.execute(sql, (srv, srv, srv, st, srv))
+        curr.fetchall()
+        conn.commit()
+        curr.close()
+        conn.close()
